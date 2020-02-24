5e29f5fe10ce9d9d355cebf8e4eed54a41c97f96
nguyen trung kien <nguyentrungkien@MacBook-Air-cua-nguyen.local>
2020-02-24 18:12:12 +0700

    testing

--- api/services/RandomUsername.js
+++ api/services/RandomUsername.js
@@ -1,13 +1,14 @@
 const request = require('request-promise');
 module.exports = async ()=>{
 	let options = {
-		url:'https://randomuser.me/api/',
-		method:'GET',
-		headers:{
-		    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.72'
-		}
+	    url:'http://names.drycodes.com/1?nameOptions=boy_names,girl_names',
+	    method:'GET',
+	    headers:{
+	    	'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.72',
+	    	
+	    }
 	};
 	let responseText = await request(options);
-	let {results:[{email}]} = JSON.parse(responseText);
-	return email.split('@')[0]
+	let responseJson = JSON.parse(responseText);
+	return responseJson[0].replace('_','.').toLowerCase();
 };
